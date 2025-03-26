502c31352ea748a942d9729ef6561bdab63fa53e
jointoclass <lessypark06@gmail.com>
2025-03-26 14:31:36 +0900

    23456

--- gameshop_jwt/src/main/java/dw/gameshop/config/WebSocketConfig.java
+++ gameshop_jwt/src/main/java/dw/gameshop/config/WebSocketConfig.java
@@ -19,9 +19,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        // /topic, /user 를 브로커의 목적지로 설정합니다.
+        // /topic, /user 를 브로커의 목적지로 설정합니다. (채녈 주제 선정)
         registry.enableSimpleBroker("/topic", "/user");
-        // 클라이언트에서 /user 로 시작하는 목적지를 구독하면, 이를 내부적으로 처리할 수 있도록 함.
+        // 클라이언트에서 /user 로 시작하는 목적지를 구독하면, 이를 내부적으로 처리할 수 있도록 함.  (개인에게 보내는 채팅에 사용)
         registry.setUserDestinationPrefix("/user");
         // 클라이언트가 보내는 메시지는 /app 으로 시작해야 함.
         registry.setApplicationDestinationPrefixes("/app");
